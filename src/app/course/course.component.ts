a68e07bf0985379f5448baf5d4389aade4c82c79
Ky Tran <youarenotalone0209@gmail.com>
2020-12-06 00:32:23 -0500

    Complete app

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,45 +1,51 @@
-import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
-import {Course} from '../model/course';
-import {
-  debounceTime,
-  distinctUntilChanged,
-  startWith,
-  tap,
-  delay,
-  map,
-  concatMap,
-  switchMap,
-  withLatestFrom,
-  concatAll, shareReplay, catchError
-} from 'rxjs/operators';
-import {merge, fromEvent, Observable, concat, throwError} from 'rxjs';
-import {Lesson} from '../model/lesson';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Course } from '../model/course';
+import { combineLatest, Observable } from 'rxjs';
+import { Lesson } from '../model/lesson';
+import { CoursesService } from '../services/courses.service';
+import { map, startWith, tap } from 'rxjs/operators';
 
+interface CourseData {
+  course: Course;
+  lessons: Lesson[];
+}
 
 @Component({
   selector: 'course',
   templateUrl: './course.component.html',
-  styleUrls: ['./course.component.css']
+  styleUrls: ['./course.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CourseComponent implements OnInit {
 
-  course: Course;
+  data$: Observable<CourseData>;
+  course$: Observable<Course>;
+  lessons$: Observable<Lesson[]>;
 
-  lessons: Lesson[];
-
-  constructor(private route: ActivatedRoute) {
-
-
-  }
+  constructor(private route: ActivatedRoute, private coursesService: CoursesService) { }
 
   ngOnInit() {
+    const courseId = parseInt(this.route.snapshot.paramMap.get('courseId'), 10);
+    const course$ = this.coursesService.loadCourseById(courseId).pipe(
+      // tslint:disable-next-line: deprecation
+      startWith(null)
+    );
+    const lessons$ = this.coursesService.loadAllCourseLessions(courseId).pipe(
+      startWith([])
+    );
 
-
-
+    // CombineLatest returns data from each observable whenever it receives new value,
+    // However, the first time calling combineLatest, it will have to wait until all observables to
+    // emit value. Therefore, I am using startWith above to emit the default value
+    this.data$ = combineLatest([course$, lessons$]).pipe(
+      map(([course, lessons]) => ({
+        course,
+        lessons,
+      })),
+      tap(console.log)
+    );
   }
-
-
 }
 
 
--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from "@angular/core";
 import { Observable } from "rxjs/internal/Observable";
 import { map, shareReplay } from "rxjs/operators";
 import { Course } from "../model/course";
+import { Lesson } from "../model/lesson";
 
 @Injectable({
   providedIn: 'root'
@@ -25,5 +26,35 @@ export class CoursesService {
       .pipe(
         shareReplay()
       );
+  }
+
+  searchLesson(search: string): Observable<Lesson[]> {
+    return this.http.get<Lesson[]>('/api/lessons', {
+      params: {
+        filter: search,
+        pageSize: '100'
+      }
+    }).pipe(
+      map(res => res['payload']),
+      shareReplay()
+    );
+  }
+
+  loadCourseById(courseId: number): Observable<Course> {
+    return this.http.get<Course>(`/api/courses/${courseId}`).pipe(
+      shareReplay()
+    );
+  }
+
+  loadAllCourseLessions(courseId: number): Observable<Lesson[]> {
+    return this.http.get<Lesson[]>('/api/lessons', {
+      params: {
+        pageSize: '1000',
+        courseId: courseId.toString()
+      }
+    }).pipe(
+      map(res => res['payload']),
+      shareReplay()
+    );
   }
 }
